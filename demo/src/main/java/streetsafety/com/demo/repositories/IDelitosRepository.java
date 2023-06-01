61bc975afc6b7b9db6a393a1f13f2e57e2bc72b6
CarlosCumpa <87941522+CarlosCumpa@users.noreply.github.com>
2023-06-01 10:31:23 -0500

    Delete demo/src/main/java/streetsafety/com/demo directory

--- demo/src/main/java/streetsafety/com/demo/repositories/IDelitosRepository.java
+++
@@ -1,7 +0,0 @@
-package streetsafety.com.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import streetsafety.com.demo.entities.Delitos;
-
-public interface IDelitosRepository extends JpaRepository<Delitos, Integer> {
-}
